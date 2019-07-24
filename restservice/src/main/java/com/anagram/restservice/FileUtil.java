0284dc83f641ca1e6fd71a6ddb7675130467af8a
stahani86 <52637779+stahani86@users.noreply.github.com>
2019-07-24 17:44:27 +0530

    Update FileUtil.java

--- restservice/src/main/java/com/anagram/restservice/FileUtil.java
+++ restservice/src/main/java/com/anagram/restservice/FileUtil.java
@@ -1,20 +1,21 @@
 package com.anagram.restservice;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FileUtil {
 
-	public static String filePath = "D:\\Jagadeesh\\parT\\SyedUSA\\wdvg-anagram\\demo\\src\\main\\resources\\words_alpha.txt";
+	public static String filePath = "words_alpha.txt";
 	
 	public static List<String> getListOfWordsFromDictionary(String filePath) {
 		List<String> lines = new ArrayList<String>();
 		try(
-			FileReader reader = new FileReader(filePath);
-			BufferedReader br = new BufferedReader(reader)
+			InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(filePath);
+			BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))
 			)
 		{
 			lines = br.lines().collect(Collectors.toList());
